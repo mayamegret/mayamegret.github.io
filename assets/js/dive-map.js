dd9fe575b151ec082acaae825b256a6a8eaa839a
mayamegret <mayamegret@gmail.com>
2026-06-18 18:48:12 -1000

    Update dive-map.js

--- assets/js/dive-map.js
+++ assets/js/dive-map.js
@@ -137,7 +137,7 @@ const locations = [
 
 const map = L.map('dive-map').setView([20, 10], 2);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
   subdomains: 'abcd', maxZoom: 19
 }).addTo(map);
