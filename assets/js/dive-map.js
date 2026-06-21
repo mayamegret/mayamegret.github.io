02012fda8a49bcfe3a2eb4c75d47d0bf95693be7
mayamegret <mayamegret@gmail.com>
2026-06-20 18:17:30 -1000

    Update dive-map.js

--- assets/js/dive-map.js
+++ assets/js/dive-map.js
@@ -41,7 +41,17 @@ const locations = [
       "Bull sharks <em>(Carcharhinus leucas)</em>",
       "Nurse sharks <em>(Ginglymostoma cirratum)</em>"
     ],
-    photos: []
+    photos: ["/images/map/JoeyMayaFiji.jpg", "/images/map/Fiji2.jpg"]
+  },
+  {
+    name: "Port Lincoln",
+    country: "Australia",
+    coords: [-34.73, 135.88],
+    type: "visit",
+    dates: "November 20-22nd, 2022",
+    description: "We came for the great white sharks, we stayed for the koalas. A really special time in my heart.",
+    species: [],
+    photos: ["/images/map/PortLincoln1.JPG", "/images/map/PortLincoln2.JPG", "/images/map/PortLincoln3.JPG", "/images/map/PortLincoln4.JPG"]
   },
   {
     name: "Cephalonia",
@@ -51,7 +61,7 @@ const locations = [
     dates: "July 22nd, 2023",
     description: "During my fieldwork in Greece monitoring nesting sea turtles and their hatchlings, I went diving! It was lovely, warm, and I saw a few nudis!",
     species: [],
-    photos: []
+    photos: ["/images/map/Cephalonia1.jpg"]
   },
   {
     name: "Isla Bastimentos",
@@ -62,6 +72,26 @@ const locations = [
     description: "Brynn, dad, and I went diving together on our family trip to Panama! It was stunning, had a great great time, even did a night dive!",
     species: [],
     photos: []
+  },
+  {
+    name: "Cartagena",
+    country: "Colombia",
+    coords: [10.39, -75.48],
+    type: "visit",
+    dates: "July 4-6th, 2024",
+    description: "",
+    species: [],
+    photos: ["/images/map/Cartagena1.JPG", "/images/map/Cartagena2.jpg", "/images/map/Cartagena3.JPG"]
+  },
+  {
+    name: "Bogotá",
+    country: "Colombia",
+    coords: [4.71, -74.07],
+    type: "visit",
+    dates: "July 30 - August 3rd, 2024",
+    description: "",
+    species: [],
+    photos: ["/images/map/Bogota1.jpg", "/images/map/Bogota2.JPG", "/images/map/Bogota3.JPG", "/images/map/Bogota4.JPG"]
   },
   {
     name: "La Jolla, San Diego",
@@ -84,150 +114,19 @@ const locations = [
     dates: "February-May 2025",
     description: "During my masters courses at UAlg, I completed my advanced open water course and completed a scientific diving course with Diogo at CCMAR. It was an amazing experience, I learned so many great skills, improved my buoyancy and trim, and learned how to conduct science underwater! From these dives, I realized how important diving is to my career as a marine biologist and it has led me down the path I am on today!",
     species: [],
-    photos: []
+    photos: ["/images/map/Sagres1.JPG", "/images/map/Sagres2.JPG"]
+  },
+  {
+    name: "Aix-en-Provence",
+    country: "France",
+    coords: [43.53, 5.45],
+    type: "visit",
+    dates: "April 10-15th, 2025",
+    description: "Dad and I went to visit family in Aix for a few days, it was so so lovely!! We ate amazing food, we explored the markets every morning, and spent the rest of our time with loved ones we hadn't seen in too long.",
+    species: [],
+    photos: ["/images/map/Aix1.jpg", "/images/map/Aix2.jpg", "/images/map/Aix5.jpg"]
   },
   {
     name: "Závora",
     country: "Mozambique",
-    coords: [-24.52, 35.20],
-    type: "dive",
-    dates: "July 2-25th, 2025",
-    description: "During my first month in Mozambique, I interned with MAR Divers where I completed 26 scientific dives focused on photo identification of manta rays, nudibranch diversity surveys, and predatory fish surveys. Additionally, my internship focused on Humpback whale migration surveys from the sand dunes and from the boat. We were so lucky to experience a whale swimming by us on one of our last dives!",
-    species: [
-      "Humpback whale <em>(Megaptera novaeangliae)</em>",
-      "Oceanic Manta Ray <em>(Mobula birostris)</em>",
-      "Reef Manta Ray <em>(Mobula alfredi)</em>"
-    ],
-    photos: []
-  },
-  {
-    name: "Guinjata Bay",
-    country: "Mozambique",
-    coords: [-24.07, 35.48],
-    type: "dive",
-    dates: "July 13th, 2025",
-    description: "Spent a night in Guinjata doing a receiver retrieval, it was a lovely time.",
-    species: [],
-    photos: []
-  },
-  {
-    name: "Závora",
-    country: "Mozambique",
-    coords: [-24.53, 35.21],
-    type: "dive",
-    dates: "January-February 2026",
-    description: "I spent 6 weeks back in Závora to collect the data for my thesis research! I completed 45 scientific dive surveys comparing three different SCUBA-based sampling methodologies for assessing the abundance and species diversity of nudibranchs across 3 different rocky reef sites. It was a spectacular time, I collected data on the size, species, depth, substrate, any additional observations, and photographs of 343 nudibranchs! I am extremely grateful for the MAR Team, especially Nakia, Emily, Ed, and Patrick for all of their help, I truly couldn't have done it without them.",
-    species: [
-      "Oceanic Manta Ray <em>(Mobula birostris)</em>",
-      "Reef Manta Ray <em>(Mobula alfredi)</em>"
-    ],
-    photos: []
-  },
-  {
-    name: "Honolulu",
-    country: "Hawai'i, USA",
-    coords: [21.31, -157.86],
-    type: "dive",
-    dates: "May 2026 - Present",
-    description: "I moved to Hawai'i as I finish up my masters thesis to get my Divemaster certification with Hawaiian Diving Adventures! It began with getting Stress & Rescue certified, and then I began my Divemaster program.",
-    species: [],
-    photos: []
-  }
-  {
-  name: "Faro",
-  country: "Portugal",
-  coords: [37.02, -7.93],
-  type: "visit",
-  dates: "September 2025 - June 2026",
-  description: "Lived in Faro while I completed my masters courses at UAlg! I had a cute apartment downtown, spent plenty of time at the beach, and was lucky enough to visit Lisbon and Nazare while living there!",
-  species: [],
-  photos: []
-},
-{
-  name: "Aix-en-Provence",
-  country: "France",
-  coords: [43.53, 5.45],
-  type: "visit",
-  dates: "April 10-15th, 2025",
-  description: "Dad and I went to visit family in Aix for a few days, it was so so lovely!! We ate amazing food, we explored the markets every morning, and spent the rest of our time with loved ones we hadn't seen in too long.",
-  species: [],
-  photos: []
-},
-{
-  name: "Port Lincoln",
-  country: "Australia",
-  coords: [-34.73, 135.88],
-  type: "visit",
-  dates: "November 20-22nd, 2022",
-  description: "We came for the great white sharks, we stayed for the koalas. A really special time in my heart.",
-  species: [],
-  photos: []
-},
-{
-  name: "Bogotá",
-  country: "Colombia",
-  coords: [4.71, -74.07],
-  type: "visit",
-  dates: "July 30 - August 3rd, 2024",
-  description: "",
-  species: [],
-  photos: []
-},
-{
-  name: "Cartagena",
-  country: "Colombia",
-  coords: [10.39, -75.48],
-  type: "visit",
-  dates: "July 4-6th, 2024",
-  description: "",
-  species: [],
-  photos: []
-}
-];
-
-const map = L.map('dive-map', { minZoom: 2 }).setView([20, 10], 2);
-
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-  attribution: '&copy; Esri &copy; National Geographic',
-  maxZoom: 16
-}).addTo(map);
-
-const diveIcon = L.divIcon({
-  html: '<div class="dive-pin">🤿</div>',
-  className: '',
-  iconSize: [32, 32],
-  iconAnchor: [16, 16],
-  popupAnchor: [0, -18]
-});
-
-const visitIcon = L.divIcon({
-  html: '<div class="visit-pin">📍</div>',
-  className: '',
-  iconSize: [28, 28],
-  iconAnchor: [14, 14],
-  popupAnchor: [0, -16]
-});
-
-function buildPopup(loc) {
-  let html = `<div class="popup-title">${loc.name}, ${loc.country}</div>`;
-  if (loc.dates) html += `<div class="popup-dates">${loc.dates}</div>`;
-  if (loc.description) html += `<div class="popup-description">${loc.description}</div>`;
-  if (loc.species && loc.species.length > 0) {
-    html += `<div class="popup-species"><strong>Species highlights:</strong><ul>`;
-    loc.species.forEach(s => html += `<li>${s}</li>`);
-    html += `</ul></div>`;
-  }
-  if (loc.photos && loc.photos.length > 0) {
-    html += `<div class="popup-photos">`;
-    loc.photos.forEach(p => html += `<img src="${p}" alt="">`);
-    html += `</div>`;
-  }
-  return html;
-}
-
-locations.forEach(loc => {
-  const icon = loc.type === 'dive' ? diveIcon : visitIcon;
-  L.marker(loc.coords, { icon })
-    .bindPopup(buildPopup(loc), { maxWidth: 280 })
-    .addTo(map);
-});
+    coords:
