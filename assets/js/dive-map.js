521fcd7b890182d217e0e2f8d4e96ab50931f186
mayamegret <mayamegret@gmail.com>
2026-06-21 18:10:51 -1000

    Update dive-map.js

--- assets/js/dive-map.js
+++ assets/js/dive-map.js
@@ -130,9 +130,19 @@ const locations = [
     dates: "April 10-15th, 2025",
     description: "Dad and I went to visit family in Aix for a few days, it was so so lovely!! We ate amazing food, we explored the markets every morning, and spent the rest of our time with loved ones we hadn't seen in too long.",
     species: [],
-    photos: ["/images/map/Aix1.jpg", "/images/map/Aix2.jpg", "/images/map/Aix5.jpg"],
+    photos: ["/images/map/Aix1.jpg", "/images/map/Aix2.jpg", "/images/map/Aix3.jpg","/images/map/Aix4.jpg", "/images/map/Aix5.jpg", "/images/map/Aix6.jpg", "/images/map/Aix7.jpg", "/images/map/Aix8.jpg"],
     galleryId: "aix"
   },
+  {
+  name: "Timbavati Reserve",
+  country: "South Africa",
+  coords: [-24.45, 31.17],
+  type: "visit",
+  dates: "June 23-30th, 2025",
+  description: "Before beginning my adventure in Mozambique, my mom and I spent a week on safari in South Africa! We did 2 game drives each day, one at dawn and one at dusk. We saw all the big 5 and had a blast!!",
+  species: [],
+  photos: ["/images/map/Safari1.JPG", "/images/map/Safari2.JPG", "/images/map/Safari3.jpg", "/images/map/Safari4.jpg", "/images/map/Safari5.JPG", "/images/map/Safari6.jpg", "/images/map/Safari7.JPG", "/images/map/Safari8.jpg", "/images/map/Safari9.JPG", "/images/map/Safari10.JPG", "/images/map/Safari11.jpg", "/images/map/Safari12.jpg", "/images/map/Safari13.JPG"]
+},
   {
     name: "Závora",
     country: "Mozambique",
@@ -169,6 +179,57 @@ const locations = [
     photos: ["/images/map/Faro1.JPG", "/images/map/Faro2.JPG", "/images/map/Faro3.jpg"],
     galleryId: "faro"
   },
+  {
+  name: "Bonaire",
+  country: "Netherlands Antilles",
+  coords: [12.18, -68.24],
+  type: "dive",
+  dates: "November 1-9th, 2025",
+  description: "",
+  species: [My family and I spent a week in Bonaire celebrating my moms birthday. We stayed in a cute house right on the water, snorkeling 3 times a day and exploring the island in between! Dad and I even went scuba diving a few times, we saw lots of squid and even a pod of dolphins 20m deep! Around the island, we saw flamingos and iguanas. A beautiful island getaway!],
+  photos: ["/images/map/Bonaire1.JPG", "/images/map/Bonaire2.JPG", "/images/map/Bonaire3.JPG", "/images/map/Bonaire4.JPG", "/images/map/Bonaire5.JPG", "/images/map/Bonaire6.JPG", "/images/map/Bonaire7.JPG", "/images/map/Bonaire8.JPG", "/images/map/Bonaire9.JPG"]
+},
+{
+  name: "Oslo",
+  country: "Norway",
+  coords: [59.91, 10.75],
+  type: "visit",
+  dates: "December 19-21st, 2025",
+  description: "",
+  species: [We kicked off our Christmas vacation in Oslo for a few days, where we enjoyed a traditional Nordic Christmas dinner and explored the cutesy little shops around the city. We even visited the MUNCH Museum and saw The Scream!],
+  photos: ["/images/map/Oslo1.JPG", "/images/map/Oslo2.jpg", "/images/map/Oslo3.JPG", "/images/map/Oslo4.JPG", "/images/map/Oslo5.jpg"]
+},
+{
+  name: "Tromsø",
+  country: "Norway",
+  coords: [69.65, 18.96],
+  type: "visit",
+  dates: "December 21-26th, 2025",
+  description: "",
+  species: [After a few days in Oslo, we flew to Tromsø, where the sun didn't rise above the horizon for the 5 days we were there. It was STUNNING. We enjoyed a fantastic view of the Northern Lights right from our porch, visited a reindeer farm, and went on a husky sleigh ride. We cooked a coq au vin for Christmas Eve dinner, and watched plenty of Christmas movies! It was truly an unforgettable Christmas!],
+  photos: ["/images/map/Tromso1.jpg", "/images/map/Tromso2.jpg", "/images/map/Tromso3.JPG", "/images/map/Tromso4.JPG"]
+},
+{
+  name: "Bergen",
+  country: "Norway",
+  coords: [60.39, 5.32],
+  type: "visit",
+  dates: "December 27-30th, 2025",
+  description: "",
+  species: [We finished our time in Norway in Bergen for a few days, where we did a beautiful boat tour of the fjords!],
+  photos: ["/images/map/Bergen1.JPG", "/images/map/Bergen2.JPG", "/images/map/Bergen3.jpg", "/images/map/Bergen4.JPG"]
+},
+{
+  name: "Copenhagen",
+  country: "Denmark",
+  coords: [55.68, 12.57],
+  type: "visit",
+  dates: "December 30th, 2025 - January 3rd, 2026",
+  description: "",
+  species: [Andrew and Bella joined the four of us in Copenhagen for a beautiful NYE! We toured the city, stopping at every cute shop and photobooth we could. We drank plenty of glögg and played many card games!],
+  photos: ["/images/map/Copenhagen1.jpg", "/images/map/Copenhagen2.JPG", "/images/map/Copenhagen3.jpg", "/images/map/Copenhagen4.JPG", "/images/map/Copenhagen5.jpeg", "/images/map/Copenhagen6.jpg", "/images/map/Copenhagen7.jpg"]
+},
+
   {
     name: "Závora",
     country: "Mozambique",
