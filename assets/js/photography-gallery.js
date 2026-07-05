f37315374360bd323939bda0c5618536789cd4ad
mayamegret <mayamegret@gmail.com>
2026-07-05 11:33:39 -1000

    Update photography-gallery.js

--- assets/js/photography-gallery.js
+++ assets/js/photography-gallery.js
@@ -1,5 +1,5 @@
 const tiles = [
-  { photos: ["/images/photography/MagazineCollage.png"], categories: ["travel", "underwater", "wildlife"] id: "honolulu", mapLink: "/map/#honolulu" },
+  { photos: ["/images/photography/MagazineCollage.png"], categories: ["travel", "underwater", "wildlife"], id: "honolulu", mapLink: "/map/#honolulu" },
   { photos: ["/images/photography/IMG_0593.jpg", "/images/photography/IMG_0599.jpg", "/images/photography/IMG_1093.jpg"], caption: "Beach scenes, Kona, Hawai'i", id: "kona", mapLink: "/map/#kona", categories: ["travel", "underwater"] },
   { photos: ["/images/photography/IMG_0406.jpg"], categories: ["underwater"] },
   { photos: ["/images/photography/IMG_0455.jpg", "/images/photography/IMG_0475.jpg"], caption: "Pencil urchins!", categories: ["underwater", "wildlife"] },
