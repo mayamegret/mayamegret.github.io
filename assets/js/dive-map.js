0342d04944784cf5ad5319468d6a4bbe26f8f0e0
mayamegret <mayamegret@gmail.com>
2026-07-15 18:57:11 -1000

    Update dive-map.js

--- assets/js/dive-map.js
+++ assets/js/dive-map.js
@@ -896,11 +896,11 @@ window.filterMap = function(type) {
 
 // Counter
 const countries = new Set(locations.map(l => l.country));
-const diveCount = locations.filter(l => l.type === 'dive').length;
 const oceansVisited = new Set(locations.map(l => l.ocean).filter(Boolean));
+const diveCount = locations.filter(l => l.type === 'dive').length;
 setTimeout(() => {
   const el = document.getElementById('map-counter');
-  if (el) el.innerHTML = `<strong>${countries.size}</strong> countries &nbsp;·&nbsp; <strong>${diveCount}</strong> dive sites &nbsp;·&nbsp; <strong>${oceansVisited.size}</strong> oceans`;
+  if (el) el.innerHTML = `<strong>${countries.size}</strong> countries &nbsp;·&nbsp; <strong>${oceansVisited.size}</strong> oceans &nbsp;·&nbsp; <strong>${diveCount}</strong> dive sites`;
 }, 100);
 
 if (window.location.hash) {
